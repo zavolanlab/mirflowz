9a962b4830ec06893d20a55e910f1b13d1979372
Alex Kanitz <alexander.kanitz@alumni.ethz.ch>
2022-07-08 17:47:57 +0200

    fix: update job script

--- jobscript.sh
+++ jobscript.sh
@@ -3,10 +3,8 @@
 echo -e "JOB ID\t$SLURM_JOBID"
 echo "=============================="
 echo -e "rule\t$JOB_NAME"
-echo -e "=============================="
+echo "=============================="
 
-ml purge
-ml Singularity
 {exec_job}
 
 echo -e "==============================\n"
