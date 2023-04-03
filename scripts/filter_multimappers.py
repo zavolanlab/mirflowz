cf08dc815d16ce4f62c965e1f09fd01c831ded7e
Iris Mestres <iris.mestrespascual@unibas.ch>
2023-04-03 08:13:56 +0200

    refactor: update script

--- scripts/filter_multimappers.py
+++ scripts/filter_multimappers.py
@@ -1,133 +1,172 @@
 #!/usr/bin/env python
 
+"""Filter multimappers in a SAM file by indel count.
+
+This script uses the pysam library to open the input SAM file and iterates
+over each alignment in it. It is assumed that the file is ordered by query
+name. For each alignment, it checks if it is a secondary or supplementary
+alignment, if it is, it skips to the next alignment. Otherwise, it adds the
+alignment to a list storing all alignments with the same query name.
+If the current query changes, it selects the best alignment(s) from the list
+of current alingments and writes them to the standard output. Current query
+and current alignments list are reset to the new query and alignment
+respectively.
+Finally, if there are any remaining alignments in the current alignments
+list and selects the best alignment(s) for those queries, writing them to
+standard output.
+
+Input:
+    input_sam_file (str): Path to the input SAM file, ordered by query name.
+
+Output:
+    standard output: Filtered SAM file
+
+Functions:
+    parse_arguments():
+        Command-line arguments parser
+    count_indels(pysam.AlignedSegment):
+        Count the number of indels in an alignment based on its CIGAR string.
+    find_best_alignments(list[pysam.AlignedSegment]):
+        Find alignments with less indels
+    write_output(list[pysam.AlignedSegment]):
+        Write the output to the standard output (stdout).
+    main(Path):
+        Filter multimappers by indels count.
+
+Usage:
+    python filter_multimappers.py -i input_sam_file
+    python filter_multimappers.py -i input_sam_file > output_sam_file
+"""
+from pathlib import Path
+from typing import List
 import argparse
 import pysam
 import sys
 
+
 def parse_arguments():
-    '''
-    Command-line arguments parser
-    '''
+    """Command-line arguments parser."""
     parser = argparse.ArgumentParser(
-        description="Script to extend pre-miRNs overhang."
+        description="Script to filter multimappers by indel counts."
         )
 
     parser.add_argument(
-        '-v','--version', 
-        action='version', 
+        '-v', '--version',
+        action='version',
         version='%(prog)s 1.0',
         help="Show program's version number and exit"
         )
 
     parser.add_argument(
-        '-i', '--input', 
-        help="Path to the gff3 input file",
-        type=str,
-        default=None,
+        '-i', '--infile',
+        help="Path to the SAM input file, sorted by query name.",
+        type=Path
         )
 
-    parser.add_argument(
-        '-o', '--output', 
-        help="Path for the gff3 final file.",
-        type=str,
-        default=None,
-        )
     return parser
 
 
-def count_matches(read):
-    '''
-    Counts the number of matches in a read based on its CIGAR string.
+def count_indels(alignment: pysam.AlignedSegment) -> int:
+    """Count the number of indels in an alignment based on its CIGAR string.
+
+    This function counts the number of indels in the alignment based on the
+    alignment CIGAR string returned by .cigartuples. Insertions are encoded as
+    1s whereas the deletions are encoded as 2.
+    Refer to the .cigartuples documentation for more information at:
+    https://pysam.readthedocs.io/en/v0.15.0/api.html#pysam.AlignedSegment.cigartuples
 
     Args:
-        read (pysam.AlignedSegment): 
-            The read to count matches for.
+        alignments(pysam.AlignedSegment): The read to count insertions for.
+
 
     Returns:
-        int: The number of matches in the read.
-    '''
-    
-    return sum([op[1] for op in read.cigar if op[0] == 0])
+        int: The sum of insertions and deletions in the alignment.
+    """
+    alignment_indels: list[int] = [op[1] for op in alignment.cigartuples if op[0] == 1 or op[0] == 2]
+    indels: int = sum(alignment_indels)
 
-def count_indels(read):
-    '''
-    Counts the number of insertions and/or deletions in a read based on its 
-    CIGAR string.
+    return indels
+
+
+def find_best_alignments(alignments: List[pysam.AlignedSegment]) -> List[pysam.AlignedSegment]:
+    """Find alignments with less indels.
+
+    This function creates a list of tuples with the alignment object and its
+    number of indels. Then, computes the minimum number of indels and returns
+    a list with the alignments that have no more than that minimum number of
+    indels.
 
     Args:
-        read (pysam.AlignedSegment): The read to count insertions for.
+        alignments (list[pysam.AlignedSegment]): alignments with the same query name
 
-    Returns:
-        int: The number of insertions in the read.
-    '''
-    return sum([op[1] for op in read.cigar if op[0] == 1 or op[0] == 2])
+    Retrns:
+        best_alignments (list[pysam.AlignedSegment]): alignments with the less indels
+    """
+    alignment_indels = [(aln, count_indels(alignment=aln)) for aln in alignments]
+    min_indels = min(alignment_indels, key=lambda x: x[1])[1]
+    best_alignments = [indelCount[0] for indelCount in alignment_indels if indelCount[1] == min_indels]
 
-def filter_multimappers(in_sam_file, out_sam_file):
-    '''
-    Filters multimappers from a SAM file and writes the filtered reads to a new
-    SAM file.
+    return best_alignments
 
-    This function scans a SAM file recording how many alignments per read there
-    are. Then, for the reads with multiple alignments (multimappers), it counts
-    the number of matches and indels it contain. Finally, it writes to a new SAM
-    file the alignments with the more matches and the less indels. If different 
-    alignments obtain the same score, all of them will be kept.
+
+def write_output(alignments: List[pysam.AlignedSegment]) -> None:
+    """Write the output to the standard output (stdout).
 
     Args:
-        in_sam_file (str): 
-            Path to the input SAM file.
-        out_sam_file (str): 
-            Path to the output SAM file.
+        alignments (list[pysam.AlignedSegment]): alignments with the same query name
+    """
+    if len(alignments) == 1:
+        sys.stdout.write(alignments[0].to_string() + '\n')
+    else:
+        for alignment in alignments:
+            sys.stdout.write(alignment.to_string() + '\n')
 
-    Returns:
-        None
-    '''
 
-    # Open input SAM file and create output SAM file
-    samfile = pysam.AlignmentFile(in_sam_file, "r")
-    out_sam = pysam.AlignmentFile(out_sam_file, "w", template=samfile)
+def main(sam_file: Path) -> None:
+    """Filter multimappers by indels count.
 
-    # Create dictionary to group reads by query name
-    read_dict = {}
-    for read in samfile:
-        # Skip secondary and supplementary alignments
-        if read.is_secondary or read.is_supplementary:
-            continue
+    Args:
+        sam_file (str): Path to the input SAM file.
+    """
+    with pysam.AlignmentFile(sam_file, "r") as samfile:
+
+        # Extract header from input SAM file and write it to stdout
+        header = samfile.header
+        sys.stdout.write(str(header))
+
+        current_query = None
+        current_alignments: list[pysam.AlignedSegment] = []
         
-        if read.query_name in read_dict:
-            read_dict[read.query_name].append(read)
-        else:
-            read_dict[read.query_name] = [read]
+        for alignment in samfile:
+ 
+            # Skip secondary and supplementary alignments
+            if alignment.is_secondary or alignment.is_supplementary:
+                continue
 
-    # Iterate over reads with the same query name
-    for read_name in read_dict:
-        reads = read_dict[read_name]
-        if len(reads) == 1:  
-            out_sam.write(reads[0])
-        else:  
-            # Create list of tuples with read object, number of matches and indels
-            read_counts = [(r, count_matches(r), count_indels(r)) for r in reads]
-            # Find the maximum counts for matches and indels
-            max_counts = max(read_counts, key=lambda x: (x[1], -x[2]))[1:]
-            # Create list of all reads with the same maximum counts
-            best_reads = [mr[0] for mr in read_counts if mr[1:] == max_counts]
-            
-            if len(best_reads) == 1:
-                out_sam.write(best_reads[0])
+            if current_query is None:
+                current_query = alignment.query_name
+
+            if current_query != alignment.query_name:
+                if len(current_alignments) == 1:
+                    write_output(alignments=current_alignments)
+                else:
+                    best_alignments = find_best_alignments(alignments=current_alignments)
+                    write_output(alignments=best_alignments)
+
+                # Reset variables
+                current_query = alignment.query_name
+                current_alignments = [alignment]
             else:
-                for best_read in best_reads:
-                    out_sam.write(best_read)
+                current_alignments.append(alignment)
 
-    # Close input and output files
-    samfile.close()
-    out_sam.close()
-
-
-
-def main():
-    filter_multimappers(args.input, args.output)
+        # Last set of alignments
+        if len(current_alignments) == 1:
+            write_output(alignments=current_alignments)
+        else:
+            best_alignments = find_best_alignments(alignments=current_alignments)
+            write_output(alignments=best_alignments)
 
 
 if __name__ == "__main__":
     args = parse_arguments().parse_args()
-    main()
+    main(sam_file=args.infile)
