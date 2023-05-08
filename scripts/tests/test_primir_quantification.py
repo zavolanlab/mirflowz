3d0181055df421005f5ca30a124b6cc16e81822d
Iris Mestres <iris.mestrespascual@unibas.ch>
2023-05-08 21:52:20 +0200

    test: change main tests

--- scripts/tests/test_primir_quantification.py
+++ scripts/tests/test_primir_quantification.py
@@ -47,12 +47,12 @@ class TestParseArguments:
     def test_no_sam_input(self, monkeypatch, bed_sam_files):
         """Call without sam input file."""
         in_bed, in_sam, out_table = bed_sam_files
+        
         with pytest.raises(SystemExit) as sysex:
             monkeypatch.setattr(
                 sys, 'argv',
                 ['primir_quantification',
                 '--bed', str(in_bed),
-                '--outfile', str(out_table),
                 ]
             )
             parse_arguments().parse_args()
@@ -67,27 +67,11 @@ class TestParseArguments:
                 sys, 'argv',
                 ['primir_quantification',
                 '--sam', str(in_sam),
-                '--outfile', str(out_table),
                 ]
             )
             parse_arguments().parse_args()
         assert sysex.value.code == 2
 
-    def test_no_output(self, monkeypatch, bed_sam_files):
-        """Call without output file."""
-        in_bed, in_sam, out_table = bed_sam_files
-
-        with pytest.raises(SystemExit) as sysex:
-            monkeypatch.setattr(
-                sys, 'argv',
-                ['primir_quantification',
-                '--bed', str(in_bed),
-                '--sam', str(in_sam),
-                ]
-            )
-            parse_arguments().parse_args()
-        assert sysex.value.code == 2
-    
     def test_correct_input(self, monkeypatch, bed_sam_files):
         """Call with the correct input files."""
         in_bed, in_sam, out_table = bed_sam_files
@@ -97,7 +81,6 @@ class TestParseArguments:
             ['primir_quantification',
             '--bed', str(in_bed),
             '--sam', str(in_sam),
-            '--outfile', str(out_table),
             ]
         )
         args = parse_arguments().parse_args()
@@ -107,86 +90,77 @@ class TestParseArguments:
 class TestMain:
     """Test 'main()' function."""
 
-    def test_main_empty_bed_file(self, monkeypatch, tmp_path, bed_sam_empty_files, bed_sam_files):
+    def test_main_empty_bed_file(self, monkeypatch, capsys, bed_sam_empty_files, bed_sam_files):
         """Test main function with an empty bed file."""
         in_empty_bed, in_empty_sam, expected_out = bed_sam_empty_files
         in_bed, in_sam, out = bed_sam_files
-
-        out_table = tmp_path/"primir_quantification"
 
         monkeypatch.setattr(
             sys, 'argv',
             ['primir_quantification',
             '--bed', str(in_empty_bed),
             '--sam', str(in_sam),
-            '--outfile', str(out_table),
             ]
         )
         args = parse_arguments().parse_args()
         main(args)
+        captured = capsys.readouterr()
 
-        with open(expected_out, 'r') as expected, open(out_table, 'r') as output:
-            assert output.read() == expected.read()
-
+        with open(expected_out, 'r') as out_file:
+            assert captured.out  == out_file.read()
     
-    def test_main_empty_sam_file(self, monkeypatch, tmp_path, bed_sam_empty_files, bed_sam_files):
+    def test_main_empty_sam_file(self, monkeypatch, capsys, bed_sam_empty_files, bed_sam_files):
         """Test main function with an empty sam file."""
         in_empty_bed, in_empty_sam, expected_out = bed_sam_empty_files
         in_bed, in_sam, out = bed_sam_files
-
-        out_table = tmp_path/"primir_quantification"
 
         monkeypatch.setattr(
             sys, 'argv',
             ['primir_quantification',
             '--bed', str(in_bed),
             '--sam', str(in_empty_sam),
-            '--outfile', str(out_table),
             ]
         )
         args = parse_arguments().parse_args()
         main(args)
+        captured = capsys.readouterr()
 
-        with open(expected_out, 'r') as expected, open(out_table, 'r') as output:
-            assert output.read() == expected.read()
+        with open(expected_out, 'r') as out_file:
+            assert captured.out == out_file.read()
 
-    def test_main_correct_input(self, monkeypatch, tmp_path, bed_sam_files):
+    def test_main_correct_input(self, monkeypatch, capsys, bed_sam_files):
         """Test main function with the correct input files."""
         in_bed, in_sam, expected_out = bed_sam_files
 
-        out_table = tmp_path/"primir_quantification"
-
         monkeypatch.setattr(
             sys, 'argv',
             ['primir_quantification',
             '--bed', str(in_bed),
             '--sam', str(in_sam),
-            '--outfile', str(out_table),
             ]
         )
         args = parse_arguments().parse_args()
         main(args)
+        captured = capsys.readouterr()
 
-        with open(expected_out, 'r') as expected, open(out_table, 'r') as output:
-            assert output.read() == expected.read()
-
-    def test_main_missing_alignment(self, monkeypatch, tmp_path, bed_sam_missing_aln_files):
+        with open(expected_out, 'r') as out_file:
+            assert captured.out == out_file.read()
+            
+    def test_main_missing_alignment(self, monkeypatch, capsys, bed_sam_missing_aln_files):
         """Test main function with missing alignments in sam file."""
         in_bed, in_sam, expected_out = bed_sam_missing_aln_files
 
-        out_table = tmp_path/"primir_quantification"
-
         monkeypatch.setattr(
             sys, 'argv',
             ['primir_quantification',
             '--bed', str(in_bed),
             '--sam', str(in_sam),
-            '--outfile', str(out_table),
             ]
         )
         args = parse_arguments().parse_args()
         main(args)
+        captured = capsys.readouterr()
 
-        with open(expected_out, 'r') as expected, open(out_table, 'r') as output:
-            assert output.read() == expected.read()
+        with open(expected_out, 'r') as out_file:
+            assert captured.out == out_file.read()
         
