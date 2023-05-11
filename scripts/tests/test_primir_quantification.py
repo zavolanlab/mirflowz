ec7b2d904f14d1f6f53fe40ecf4650aa40972f97
Iris Mestres <iris.mestrespascual@unibas.ch>
2023-05-11 13:20:27 +0200

    test: update tests

--- scripts/tests/test_primir_quantification.py
+++ scripts/tests/test_primir_quantification.py
@@ -15,21 +15,20 @@ from scripts.primir_quantification import (
 
 
 @pytest.fixture
-def bed_sam_empty_files():
-    """Import path to empty bed test file."""
-    in_bed_empty = Path("files/in_bed_empty.bed")
-    in_sam_empty = Path("files/in_sam_empty.sam")
-    out_empty_table = Path("files/out_empty_primir_quantification")
+def empty_file():
+    """Import path to empty file."""
+    empty_file = Path("files/empty_file")
+    sam_empty_file = Path("files/in_sam_empty.sam")
 
-    return in_bed_empty, in_sam_empty, out_empty_table
+    return empty_file, sam_empty_file
 
 
 @pytest.fixture
 def bed_sam_files():
-    """Import path to test files with correct content."""
+    """Import path to test BAM/SAM files with full content."""
     in_bed = Path("files/in_intersection.bed")
     in_sam = Path("files/in_alignments.sam")
-    out_table = Path("files/out_primir_quantification")
+    out_table = Path("files/sam_primir_quantification")
 
     return in_bed, in_sam, out_table
 
@@ -39,61 +38,114 @@ def bed_sam_missing_aln_files():
     """Import path to test files with missing alignments."""
     in_bed = Path("files/in_intersection.bed")
     in_sam = Path("files/in_alignments_missing.sam")
-    out_table = Path("files/out_missing_alns_primir_quantification")
+    out_table = Path("files/sam_missing_alns_primir_quantification")
 
     return in_bed, in_sam, out_table
 
 
 @pytest.fixture
 def bed_sam_no_extension_files():
-    """Import path to test files with no extension on pri-miRs names."""
+    """Import path to test files with no extension on features names."""
     in_bed = Path("files/in_intersection_no_extension.bed")
     in_sam = Path("files/in_alignments.sam")
-    out_table = Path("files/out_primir_quantification")
+    out_table = Path("files/no_extension_primir_quantification")
 
     return in_bed, in_sam, out_table
+
+
+@pytest.fixture
+def bed_extension_id_files():
+    """Import path to test files with extension and reads id."""
+    in_bed = Path("files/in_intersection.bed")
+    in_sam = Path("files/in_alignments.sam")
+    out_table = Path("files/extension_id_primir_quantification")
+
+    return in_bed, in_sam, out_table
+
+
+@pytest.fixture
+def bed_id_files():
+    """Import path to test files with reads id."""
+    in_bed = Path("files/in_intersection_no_extension.bed")
+    in_sam = Path("files/in_alignments.sam")
+    out_table = Path("files/id_primir_quantification")
+
+    return in_bed, in_sam, out_table
+
+
+@pytest.fixture
+def bed_id_missing_aln_files():
+    """Import path to test files with missing alignments and reads id."""
+    in_bed = Path("files/in_intersection_no_extension.bed")
+    in_sam = Path("files/in_alignments_missing.sam")
+    out_table = Path("files/missing_aln_id_primir_quantification")
+
+    return in_bed, in_sam, out_table
+
+
+@pytest.fixture
+def bed_no_sam_files():
+    """Import path to test files with no sam file."""
+    in_bed = Path("files/in_intersection_no_extension.bed")
+    out_table = Path("files/no_sam_primir_quantification")
+
+    return in_bed, out_table
+    
 
 
 class TestParseArguments:
     """Test 'parse_arguments()' function."""
 
-    def test_no_sam_input(self, monkeypatch, bed_sam_files):
-        """Call without sam input file."""
-        in_bed, in_sam, out_table = bed_sam_files
-
+    def test_no_input(self, monkeypatch):
+        """Call without input file."""
         with pytest.raises(SystemExit) as sysex:
             monkeypatch.setattr(
                 sys, 'argv',
                 ['primir_quantification',
-                 '--bed', str(in_bed),
-                 ]
-            )
-            parse_arguments().parse_args()
-        assert sysex.value.code == 2
-
-    def test_no_bed_input(self, monkeypatch, bed_sam_files):
-        """Call without bed input file."""
-        in_bed, in_sam, out_table = bed_sam_files
-
-        with pytest.raises(SystemExit) as sysex:
-            monkeypatch.setattr(
-                sys, 'argv',
-                ['primir_quantification',
-                 '--sam', str(in_sam),
                  ]
             )
             parse_arguments().parse_args()
         assert sysex.value.code == 2
 
     def test_correct_input(self, monkeypatch, bed_sam_files):
-        """Call with the correct input files."""
+        """Call with the correct input file."""
         in_bed, in_sam, out_table = bed_sam_files
 
         monkeypatch.setattr(
             sys, 'argv',
             ['primir_quantification',
-             '--bed', str(in_bed),
+             str(in_bed),
+             ]
+        )
+        args = parse_arguments().parse_args()
+        assert isinstance(args, argparse.Namespace)
+    
+    def test_too_many_input_files(self, monkeypatch, bed_sam_files):
+        """Call with too many input file."""
+        in_bed, in_sam, out_table = bed_sam_files
+        
+        with pytest.raises(SystemExit) as sysex:
+            monkeypatch.setattr(
+                sys, 'argv',
+                ['primir_quantification',
+                 str(in_bed), str(in_bed),
+                 ]
+            )
+            parse_arguments().parse_args()
+        assert sysex.value.code == 2
+    
+    def test_all_input(self, monkeypatch, bed_sam_files):
+        """Call with the correct input file."""
+        in_bed, in_sam, out_table = bed_sam_files
+
+        monkeypatch.setattr(
+            sys, 'argv',
+            ['primir_quantification',
+             str(in_bed),
              '--sam', str(in_sam),
+             '--id', "name",
+             '--feat-extension',
+             '--read-ids',
              ]
         )
         args = parse_arguments().parse_args()
@@ -103,53 +155,53 @@ class TestParseArguments:
 class TestMain:
     """Test 'main()' function."""
 
-    def test_main_empty_bed_file(self, monkeypatch, capsys, bed_sam_empty_files, bed_sam_files):
+    def test_main_empty_bed_file(self, monkeypatch, capsys, empty_file, bed_sam_files):
         """Test main function with an empty bed file."""
-        in_empty_bed, in_empty_sam, expected_out = bed_sam_empty_files
+        empty_file, empty_sam = empty_file
         in_bed, in_sam, out = bed_sam_files
 
         monkeypatch.setattr(
             sys, 'argv',
             ['primir_quantification',
-             '--bed', str(in_empty_bed),
-             '--sam', str(in_sam),
+             str(empty_file),
              ]
         )
         args = parse_arguments().parse_args()
         main(args)
         captured = capsys.readouterr()
 
-        with open(expected_out, 'r') as out_file:
+        with open(empty_file, 'r') as out_file:
             assert captured.out == out_file.read()
 
-    def test_main_empty_sam_file(self, monkeypatch, capsys, bed_sam_empty_files, bed_sam_files):
+    def test_main_empty_sam_file(self, monkeypatch, capsys, empty_file, bed_sam_files):
         """Test main function with an empty sam file."""
-        in_empty_bed, in_empty_sam, expected_out = bed_sam_empty_files
+        empty_file, empty_sam = empty_file
         in_bed, in_sam, out = bed_sam_files
 
         monkeypatch.setattr(
             sys, 'argv',
             ['primir_quantification',
-             '--bed', str(in_bed),
-             '--sam', str(in_empty_sam),
+             str(in_bed),
+             '--sam', str(empty_sam),
              ]
         )
         args = parse_arguments().parse_args()
         main(args)
         captured = capsys.readouterr()
 
-        with open(expected_out, 'r') as out_file:
+        with open(empty_file, 'r') as out_file:
             assert captured.out == out_file.read()
 
-    def test_main_correct_input(self, monkeypatch, capsys, bed_sam_files):
+    def test_main_complete_sam_input(self, monkeypatch, capsys, bed_sam_files):
         """Test main function with the correct input files."""
         in_bed, in_sam, expected_out = bed_sam_files
 
         monkeypatch.setattr(
             sys, 'argv',
             ['primir_quantification',
-             '--bed', str(in_bed),
+             str(in_bed),
              '--sam', str(in_sam),
+             '--feat-extension',
              ]
         )
         args = parse_arguments().parse_args()
@@ -166,8 +218,9 @@ class TestMain:
         monkeypatch.setattr(
             sys, 'argv',
             ['primir_quantification',
-             '--bed', str(in_bed),
+             str(in_bed),
              '--sam', str(in_sam),
+             '--feat-extension',
              ]
         )
         args = parse_arguments().parse_args()
@@ -178,14 +231,89 @@ class TestMain:
             assert captured.out == out_file.read()
 
     def test_main_no_extension(self, monkeypatch, capsys, bed_sam_no_extension_files):
-        """Test main function with no extension in pri-miRs names."""
+        """Test main function with no extension in features names."""
         in_bed, in_sam, expected_out = bed_sam_no_extension_files
 
         monkeypatch.setattr(
             sys, 'argv',
             ['primir_quantification',
-             '--bed', str(in_bed),
+             str(in_bed),
              '--sam', str(in_sam),
+             ]
+        )
+        args = parse_arguments().parse_args()
+        main(args)
+        captured = capsys.readouterr()
+
+        with open(expected_out, 'r') as out_file:
+            assert captured.out == out_file.read()
+
+    def test_main_id_extension(self, monkeypatch, capsys, bed_extension_id_files):
+        """Test main function with extension in feature name and read names."""
+        in_bed, in_sam, expected_out = bed_extension_id_files
+
+        monkeypatch.setattr(
+            sys, 'argv',
+            ['primir_quantification',
+             str(in_bed),
+             '--sam', str(in_sam),
+             '--feat-extension',
+             '--read-ids',
+             ]
+        )
+        args = parse_arguments().parse_args()
+        main(args)
+        captured = capsys.readouterr()
+
+        with open(expected_out, 'r') as out_file:
+            assert captured.out == out_file.read()
+
+    def test_main_id(self, monkeypatch, capsys, bed_id_files):
+        """Test main function with read names."""
+        in_bed, in_sam, expected_out = bed_id_files
+
+        monkeypatch.setattr(
+            sys, 'argv',
+            ['primir_quantification',
+             str(in_bed),
+             '--sam', str(in_sam),
+             '--read-ids',
+             ]
+        )
+        args = parse_arguments().parse_args()
+        main(args)
+        captured = capsys.readouterr()
+
+        with open(expected_out, 'r') as out_file:
+            assert captured.out == out_file.read()
+
+    def test_main_id_missing_alns(self, monkeypatch, capsys, bed_id_missing_aln_files):
+        """Test main function with read names and missing alignments."""
+        in_bed, in_sam, expected_out = bed_id_missing_aln_files
+
+        monkeypatch.setattr(
+            sys, 'argv',
+            ['primir_quantification',
+             str(in_bed),
+             '--sam', str(in_sam),
+             '--read-ids',
+             ]
+        )
+        args = parse_arguments().parse_args()
+        main(args)
+        captured = capsys.readouterr()
+
+        with open(expected_out, 'r') as out_file:
+            assert captured.out == out_file.read()
+
+    def test_main_no_sam_file(self, monkeypatch, capsys, bed_no_sam_files):
+        """Test main function with read names."""
+        in_bed, expected_out = bed_no_sam_files
+
+        monkeypatch.setattr(
+            sys, 'argv',
+            ['primir_quantification',
+             str(in_bed),
              ]
         )
         args = parse_arguments().parse_args()
