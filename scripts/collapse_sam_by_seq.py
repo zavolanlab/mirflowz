ea17c6edccd2567adc9c44af8f06dd9f88694874
Iris Mestres <iris.mestrespascual@unibas.ch>
2023-05-16 13:47:23 +0200

    refactor: apply requested changes

--- scripts/collapse_sam_by_seq.py
+++ scripts/collapse_sam_by_seq.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 
-"""Collapse alignments in a SAM file by query sequence and starting position.
+"""Collapse reads in a SAM file by alignment and query sequence.
 
 Collapse alignments in a SAM file by query sequence and starting position, and
 write the collapsed alignment to the standard output. The process is divided
 into two steps: first, alignments with the same sequence and starting position
 are grouped into a list. Second, the alignments in each list are combined into
-a single alignment. The new alignment is given a name starting with "set_"
-followed by the number of alignments collapsed, and the "HI" tag is reset to 1.
-Two new tags are also added: the "XC" tag is the count of collapsed alignments
-divided by the "NH" tag, and the "ZS" tag contains a list of all the alignment
-IDs that are combined to create the new one.
+a single alignment. The new alignment is given a name build of the number of
+alignments collapsed and the indicated integer, and the "HI" tag is reset to 1. 
+Two new tags may be also add: the "XC" tag is the count of collapsed alignments
+divided by the "NH" tag. If the "NH" tag is missing, the value 1 is used. If
+`--read-ids` is set, the "ZS" tag will be add. This new tag contains a string
+with all the alignment IDs that are combined to create the new one separted by
+an underscore.
 """
 
 import argparse
@@ -35,26 +37,34 @@ def parse_arguments():
         'infile',
         help="Path to the SAM input file, sorted by query name.",
         type=Path
+    ),
+    parser.add_argument(
+        '--read-ids',
+        help="Include read IDs of the collpased alignments in the new tag ZS. \
+            Default: %(default)s.",
+        action='store_true',
+        default= False
     )
 
     return parser
 
 
-def collapse_alignments(alns: list[pysam.AlignedSegment], aln: int) -> pysam.AlignedSegment:
-    """Collapse alignments list into a single alignment.
+def collapse_alignments(alns: list[pysam.AlignedSegment], aln: int, reads: bool=False) -> pysam.AlignedSegment:
+    """Collapse an alignments list into a single alignment.
 
     Combine the alignments in the list into a single alignment. The new
-    alignment is given a name starting with "set_" followed by the number of
-    alignments collapsed, and the "HI" tag is reset to 1. Two new tags are also
-    added: the "XC" tag is the count of collapsed alignments divided by the
-    "NH" tag. If the "NH" tag is missing, the value 1 is used. The "ZS" tag
-    contains a list of all the alignment IDs that are combined to create
-    the new one. If the list contains a single alignment, only the 
-    aggregation of the new tags is made.
+    alignment is given a name build of the number of alignments collapsed and
+    the indicated integer, and the "HI" tag is reset to 1. Two new tags may be
+    also add: the "XC" tag is the count of collapsed alignments divided by the
+    "NH" tag. If the "NH" tag is missing, the value 1 is used. If `reads` is
+    set to True, the "ZS" tag will be add. This new tag contains a string with
+    all the alignment IDs that are combined to create the new one separted by
+    an underscore.
 
     Args:
         alns: alignments with the same query sequence
         aln: integer to define the new alignment
+        reads: flag to do or do not add the new tag
 
     Retrns:
         new_alignment: collapsed alignment
@@ -70,8 +80,9 @@ def collapse_alignments(alns: list[pysam.AlignedSegment], aln: int) -> pysam.Ali
         xc_count = 1 / nh_tag
         new_alignment.set_tags([("NH", nh_tag),
                                 ("MD", new_alignment.get_tag("MD")),
-                                ("XC", xc_count), 
-                                ("ZS", new_alignment.query_name)])
+                                ("XC", xc_count)])
+        if reads:
+            new_alignment.set_tag("ZS", new_alignment.query_name)
 
     else:
         xc_count = len(alns) / nh_tag
@@ -79,27 +90,24 @@ def collapse_alignments(alns: list[pysam.AlignedSegment], aln: int) -> pysam.Ali
         new_alignment.set_tags([("NH", nh_tag),
                                 ("MD", new_alignment.get_tag("MD")),
                                 ("XC", xc_count), 
-                                ("HI", 1), 
-                                ("ZS", "_".join(read_ids))])
+                                ("HI", 1)])
+        if reads:
+            new_alignment.set_tag("ZS", "_".join(read_ids))
         
-    new_alignment.query_name = f'set_{len(alns)}.{aln}'
+    new_alignment.query_name = f'{len(alns)}.{aln}'
 
     return new_alignment
 
 
-def main(sam_file: Path) -> None:
-    """Collapse alignments by query sequence and starting position.
-
-    Args:
-        sam_file: Path to the input SAM file.
-    """
-    with pysam.AlignmentFile(sam_file, 'r') as samfile:
+def main(args) -> None:
+    """Collapse reads by alignment and query sequence."""
+    with pysam.AlignmentFile(args.infile, 'r') as samfile:
 
         sys.stdout.write(str(samfile.header))
 
         try:
             aln = next(samfile)
-            current_seq = f'{aln.query_sequence}_{aln.reference_start}'
+            current_seq = f'{aln.query_sequence}_{aln.reference_start}_{aln.cigar}_{aln.get_tag("MD")}_{aln.flag & 0x04}'
             current_alignments = [aln]
             count = 1
 
@@ -108,20 +116,20 @@ def main(sam_file: Path) -> None:
 
         for aln in samfile:
 
-            if current_seq == f'{aln.query_sequence}_{aln.reference_start}':
+            if current_seq == f'{aln.query_sequence}_{aln.reference_start}_{aln.cigar}_{aln.get_tag("MD")}_{aln.flag & 0x04}':
                 current_alignments.append(aln)
             
             else:
-                collapsed_aln = collapse_alignments(alns=current_alignments, aln=count)
+                collapsed_aln = collapse_alignments(alns=current_alignments, aln=count, reads=args.read_ids)
                 sys.stdout.write(collapsed_aln.to_string() + '\n')
                 
-                current_seq = f'{aln.query_sequence}_{aln.reference_start}'
+                current_seq = f'{aln.query_sequence}_{aln.reference_start}_{aln.cigar}_{aln.get_tag("MD")}_{aln.flag & 0x04}'
                 current_alignments = [aln]
                 count += 1
 
-        collapsed_aln = collapse_alignments(alns=current_alignments, aln=count)
+        collapsed_aln = collapse_alignments(alns=current_alignments, aln=count, reads=args.read_ids)
         sys.stdout.write(collapsed_aln.to_string() + '\n')
         
 if __name__ == "__main__":
     args = parse_arguments().parse_args() # pragma: no cover
-    main(sam_file=args.infile) # pragma: no cover
+    main(args) # pragma: no cover
