16f6e476eb964d7f53ea50ee03c4d7313ddf2b9c
Alex Kanitz <alexander.kanitz@alumni.ethz.ch>
2024-05-19 20:42:22 +0200

    fix: fix & refactor miRNA extension script

--- scripts/mirna_extension.py
+++ scripts/mirna_extension.py
@@ -1,74 +1,118 @@
 #!/usr/bin/env python
-
-"""Extend miRNAs start and end coordinates by n nucleotides.
-
-This script uses the class MirnaExtension to extend the start and end
-coordiantes of the mature miRNAs.This class is build upon two methods. The
-first one, turns a GFF3 file into an in-memory database using gffutils. The
-second method, iterares over all primary miRNAS in the database to extend
-its mature forms' end and start cooridnates by n nucleotides without exceeding
-the chromosome boundaries. Thereafter, the primary miRNAs star/end coordinates
-will also be extened if and only if, the mature miRNA coordinates exceeds the
-primary miRNA ones; this extension will be recorded as _-y and/or _+x being the
-the extension on the 5' and 3' respectively. Finally, two different annotation
-files will be created; one for the primary miRNAs and one for the mature forms.
-
-Usage:
-    mirna_extension.py GFF3 [--outdir Path] [-e int] [--chr Path]
-    GFF3 > mirna_extension.py [--outdir Path] [-e int] [--chr Path]
-"""
+"""Extend miRNA start and end coordinates."""
 
 import argparse
 from pathlib import Path
-import sys
-from typing import Dict, Optional
+from typing import Optional
+
 import gffutils  # type: ignore
 
 
 class MirnaExtension:
-    """Class to extend miRNAs start and end coordinates.
+    """Class for extending miRNA start and end coordinates.
 
     Attributes:
-        db:
-            in-memory database of the GFF3 file.
-
-    Methods:
-        load_gff_file:
-            creates an in-memory db from an input file
-        extend_mirnas:
-            extends the start and end of the mature miRNAs by n nucleotides
+        db: In-memory database of the input GFF3 records.
+        db: In-memory database of the updated GFF3 records.
+        seq_lengths: Dictionary mapping reference sequence IDs to their lengths.
     """
 
     def __init__(self) -> None:
         """Initialize class."""
-        self.db = None
+        self.db: Optional[gffutils.FeatureDB] = None
+        self.db_out: Optional[gffutils.FeatureDB] = None
+        self.seq_lengths: Optional[dict[str, int]] = None
 
-    def load_gff_file(self, gff_file: Optional[Path] = None) -> None:
-        """Load GFF3 file.
-
-        This method uses the gffutils package to create and in-memory database
-        of the GFF3 file.
+    def set_db(self, path: Path) -> None:
+        """Load GFF3 file into `gffutils.FeatureDB`.
 
         Args:
-            gff_file:
-                path to the input GFF3 file. If None, input is taken from the
-                standard input (stdin).
+            gff_file: Path to a GFF3 file.
         """
-        if gff_file is None:
+        try:
             self.db = gffutils.create_db(
-                sys.stdin, dbfn=":memory:", force=True, keep_order=True
+                str(path),
+                dbfn=":memory:",
+                force=True,
+                keep_order=True
             )
+        except gffutils.exceptions.EmptyInputError:
+            pass
+
+    def set_seq_lengths(self, path: Optional[Path] = None) -> None:
+        """Set the reference sequence lengths.
+
+        Args:
+            path: Path to a tabulated file containing the names of reference sequences
+                and the corresponding total length, in bases/basepairs.
+        """
+        self.seq_lengths = {}
+        if path is None:
+            for _id in self.db.seqids():  # type: ignore
+                self.seq_lengths[_id] = max(
+                    record.end for record in self.db.region(_id)  # type: ignore
+                )
         else:
-            self.db = gffutils.create_db(
-                str(gff_file), dbfn=":memory:", force=True, keep_order=True
+            with open(path, encoding="utf-8") as _file:
+                for line in _file:
+                    ref_seq, length = line.strip().split("\t")
+                    try:
+                        self.seq_lengths[ref_seq] = int(length)
+                    except ValueError as exc:
+                        raise ValueError(
+                            f'Invalid length: "{length}" for sequence "{ref_seq}";'
+                            ' integer value expected'
+                        ) from exc
+
+    def process_precursor(
+        self,
+        precursor: gffutils.Feature,
+        n: int = 6
+    ) -> list[gffutils.Feature]:
+        assert isinstance(self.seq_lengths, dict)
+
+        pre_orig_start = precursor.start
+        pre_orig_end = precursor.end
+
+        matures = list(
+            self.db.region(  # type: ignore
+                strand=precursor.strand,
+                seqid=precursor.seqid,
+                start=precursor.start,
+                end=precursor.end,
+                featuretype="miRNA",
+                completely_within=True,
             )
+        )
+        for mir in matures:
+            try:
+                if self.seq_lengths[mir.seqid] < mir.end:
+                    raise ValueError(
+                        "The provided GFF3 miRNA annotations and reference sequence"
+                        f" lengths are incompatible: end coordinate {mir.end} of miRNA"
+                        f' "{mir.id}" exceeds length of reference sequence '
+                        f' "{mir.seqid}" ({self.seq_lengths[mir.seqid]} nt)'
+                    )
+            except KeyError as exc:
+                raise KeyError(
+                    "The provided GFF3 miRNA annotations and reference sequence lengths"
+                    f' are incompatible: reference sequence "{mir.seqid}" of miRNA'
+                    f' "{mir.id}" is not available in the provided reference sequence'
+                    " lengths table"
+                ) from exc
+            mir.start = max(mir.start - n, 0)
+            mir.end = min(mir.end + n, self.seq_lengths[mir.seqid])
+
+        precursor.start = min(precursor.start, min(mir.start for mir in matures))
+        precursor.end = max(precursor.end, max(mir.end for mir in matures))
+        precursor.attributes["Name"][0] += f"_-{pre_orig_start - precursor.start}"
+        precursor.attributes["Name"][0] += f"_+{precursor.end - pre_orig_end}"
+
+        return [precursor] + matures
 
     def extend_mirnas(
         self,
-        primir_out: Path,
-        mir_out: Path,
         n: int = 6,
-        seq_lengths: Optional[dict[str, int]] = None,
     ) -> None:
         """Extend miRNAs start and end coordinates.
 
@@ -79,159 +123,132 @@ class MirnaExtension:
         If provided, the elongation will take into account the chromosome size.
 
         Args:
-            primir_out:
-                path to the output extended precursor miRNA file.
-            mir_out:
-                path to the output extended mature miRNA file.
-            n:
-                number of nucleotides to extend miRs start and end coordinates.
-            seq_lengths:
-                a dictionary that maps reference sequence IDs to their lengths
-                (in  nucleotides). If None, sequence lengths are inferred from
-                the input GFF3 file.
+            n: Number of nucleotides to extend miRs start and end coordinates.
         """
-        assert isinstance(self.db, gffutils.interface.FeatureDB)
+        assert isinstance(self.db, gffutils.FeatureDB)
 
-        # Set end boundary
-        if seq_lengths is None:
-            seq_lengths = {}
-            for seqid in self.db.seqids():
-                seq_lengths[seqid] = max(
-                    rec.end for rec in self.db.region(seqid)
-                )
+        feats_updated: list[gffutils.Feature] = []
 
-        with (
-            open(primir_out, "w", encoding="utf-8") as primir,
-            open(mir_out, "w", encoding="utf-8") as mirna,
+        for pre in self.db.features_of_type(  # type: ignore
+            "miRNA_primary_transcript"
         ):
-            for primary_mirna in self.db.features_of_type(
-                "miRNA_primary_transcript"
-            ):
-                seqid = primary_mirna.seqid
-                min_start = primary_mirna.start
-                max_end = primary_mirna.end
+            feats_updated.extend(self.process_precursor(precursor=pre, n=n))
 
-                mature_miRNAs = list(
-                    self.db.region(
-                        seqid=seqid,
-                        start=min_start,
-                        end=max_end,
-                        featuretype="miRNA",
-                        completely_within=True,
-                    )
-                )
+        self.db_out = gffutils.create_db(
+            feats_updated,
+            dbfn=":memory:",
+            force=True,
+            keep_order=True,
+        )
 
-                if mature_miRNAs:
-                    for mature_mir in mature_miRNAs:
-                        if mature_mir.start - n > 0:
-                            mature_mir.start -= n
-                        else:
-                            mature_mir.start = 0
+    def write_gff(self, path: Path, feature_type: Optional[str] = None) -> None:
+        """Write features to a GFF3 file.
 
-                        if mature_mir.end + n < seq_lengths[seqid]:
-                            mature_mir.end += n
-                        else:
-                            mature_mir.end = seq_lengths[seqid]
-
-                        min_start = min(min_start, mature_mir.start)
-                        max_end = max(max_end, mature_mir.end)
-
-                        mirna.write(str(mature_mir) + "\n")
-
-                    start_diff = primary_mirna.start - min_start
-                    end_diff = max_end - primary_mirna.end
-
-                    primary_mirna.start = min_start
-                    primary_mirna.end = max_end
-
-                    primary_mirna.attributes["Name"][0] += f"_-{start_diff}"
-                    primary_mirna.attributes["Name"][0] += f"_+{end_diff}"
-
-                primir.write(str(primary_mirna) + "\n")
+        Args:
+            path: Path to the output file.
+            feature_type: Feature type to write. If `None`, all features will be
+                written.
+        """
+        with open(path, "w", encoding="utf-8") as _file:
+            if not isinstance(self.db_out, gffutils.FeatureDB):
+                _file.write("")
+            elif feature_type is None:
+                for feature in self.db_out.all_features():  # type: ignore
+                    _file.write(str(feature) + "\n")
+            else:
+                for feature in self.db_out.features_of_type(  # type: ignore
+                    feature_type
+                ):
+                    _file.write(str(feature) + "\n")
 
 
 def parse_arguments():
-    """Command-line arguments parser."""
+    """Parse command-line arguments."""
+    description = """Extend miRNA start and end coordinates.
+
+Extends mature miRNA start and end coordinates in a GFF3 file by the indicated number of
+nucleotides, but not exceeding chromosome boundaries. If the extension causes the mature
+miRNA coordinates to exceed the boundaries of the corresponding precursor (see note in
+"Constraints" below), the precursor coordinates will be extended accordingly. In this
+case, the precursor name will be appended with "_-n" and/or "_+m", where n and m
+represent the extensions on the 5' and 3' end, respectively. The modified mature miRNA
+and precursor annotations will be written to separate GFF3 files.
+
+Constraints:
+The script was written for GFF3 files obtained from miRBase (http://www.mirbase.org/).
+The following assumptions are made:
+- The input GFF3 file contains miRNA annotations for a single species.
+- The input GFF3 file contains features of type "miRNA_primary_transcript" (referred to
+  as "precursors") and "miRNA" (referred to as "mature miRNAs").
+- The input GFF3 file contains a Name attribute for each precursor.
+- Each precursor contains one or more mature miRNAs.
+- Each mature miRNA is a child of exactly one precursor and is completely within the
+  boundaries of the precursor.
+"""
     parser = argparse.ArgumentParser(
-        description="Script to extend miRNAs start and end coordinates."
+        description=description,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
     )
     parser.add_argument(
         "-v",
         "--version",
         action="version",
-        version="%(prog)s 1.0",
-        help="Show program's version number and exit",
+        version="%(prog)s 1.1.0",
+        help="show program's version number and exit",
     )
     parser.add_argument(
         "input",
-        help="Path to the GFF3 annotation file. If not provided, the input \
-             will be read from the standard input.",
-        type=Path,
-    )
-    parser.add_argument(
-        "--outdir",
-        help="Path to the output directory. Default: %(default)s.",
-        default=Path.cwd(),
+        help="path to the GFF3 annotation file",
         type=Path,
     )
     parser.add_argument(
         "-e",
         "--extension",
-        help="Number of nucleotides to extend the coordinates. Default: \
-                %(default)d.",
+        help=(
+            "number of nucleotides by which to extend the miRNA boundaries; default:"
+            " %(default)d"
+        ),
         default=6,
         type=int,
+        metavar="INT",
     )
     parser.add_argument(
         "--chr",
-        help="Path to the tabulated file containing the chromosome and its \
-            length in basepairs. If not provided, the length will be set to \
-            the biggest coordinate of the last miRNA primary transcript.",
+        help=(
+            "path to a tabulated file containing the chromosome names and their"
+            " lengths, in basepairs; if not provided, the sequence lengths will be"
+            " set to the most distal 3' boundary of the miRNA precursors annotated"
+            " for each chromosome in the input GFF3 file; default: %(default)s"
+        ),
         default=None,
         type=Path,
+        metavar="PATH",
     )
-
+    parser.add_argument(
+        "--outdir",
+        help="path to the output directory; default: %(default)s",
+        type=Path,
+        default=Path.cwd(),
+        metavar="PATH",
+    )
     return parser
 
 
-def main(arguments) -> None:
+def main(args) -> None:
     """Extend miRNAs start/end coordinates."""
-    outdir = Path(arguments.outdir)
-    outdir.mkdir(parents=True, exist_ok=True)
-
-    primir_out = outdir / (
-        f"extended_primir_annotation_{arguments.extension}_nt.gff3"
+    args.outdir.mkdir(parents=True, exist_ok=True)
+    mirna_extension = MirnaExtension()
+    mirna_extension.set_db(path=args.input)
+    if isinstance(mirna_extension.db, gffutils.FeatureDB):
+        mirna_extension.set_seq_lengths(path=args.chr)
+        mirna_extension.extend_mirnas(n=args.extension)
+    mirna_extension.write_gff(
+        path=args.outdir / f"extended_primir_annotation_{args.extension}_nt.gff3",
+        feature_type="miRNA_primary_transcript",
     )
-    mir_out = (
-        outdir / f"extended_mirna_annotation_{arguments.extension}_nt.gff3"
-    )
-
-    with open(arguments.input, encoding="utf-8") as in_file:
-        if len(in_file.read()) == 0:
-            with (
-                open(primir_out, "w", encoding="utf-8") as primir,
-                open(mir_out, "w", encoding="utf-8") as mir,
-            ):
-                primir.write("")
-                mir.write("")
-                return
-
-    # Create dictionary with the ref. sequence length
-    seq_lengths: Optional[Dict] = None
-    if arguments.chr:
-        seq_lengths = {}
-        with open(arguments.chr, encoding="utf-8") as f:
-            for line in f:
-                ref_seq, length = line.strip().split("\t")
-                seq_lengths[ref_seq] = int(length)
-
-    m = MirnaExtension()
-    m.load_gff_file(arguments.input)
-    m.extend_mirnas(
-        n=arguments.extension,
-        seq_lengths=seq_lengths,
-        primir_out=primir_out,
-        mir_out=mir_out,
+    mirna_extension.write_gff(
+        path=args.outdir / f"extended_mirna_annotation_{args.extension}_nt.gff3",
+        feature_type="miRNA",
     )
 
 
