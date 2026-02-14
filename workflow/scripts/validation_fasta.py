b5fea03fb5c19f171b7c1fad3286177d2061c1a3
deliaBlue <iris.mepa13@gmail.com>
2026-02-14 14:00:03 +0100

    style: set lines to <80 chars

--- workflow/scripts/validation_fasta.py
+++ workflow/scripts/validation_fasta.py
@@ -114,8 +114,9 @@ one per line in a separate text file.
 
     if args.mode and not args.filter:
         parser.error(
-            "Filter argument (`--filter`, `-f`) is required when using the mode"
-            " argument (`--mode`, `-m`). See `--help` for more information."
+            "Filter argument (`--filter`, `-f`) is required when using the"
+            " mode argument (`--mode`, `-m`). See `--help` for more"
+            " information."
         )
 
     return args
